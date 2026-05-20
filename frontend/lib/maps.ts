9bbf66032ed06cf77afee93667e686c7fd91eb80
MF <m_fritsche@outlook.com>
2026-05-20 16:32:33 +0200

    Delete frontend directory

--- frontend/lib/maps.ts
+++
@@ -1,8 +0,0 @@
-export const loadGoogleMaps = async () => {
-  const loader = new google.maps.plugins.loader.Loader({
-    apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
-    version: "weekly",
-  });
-
-  return loader.importLibrary("maps");
-};
